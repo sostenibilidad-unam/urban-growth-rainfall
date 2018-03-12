c934590d234881894e3cb47a2044ee540da68bb9
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-03-12 17:49:13 -0600

    write to output file, usage in comments

--- extract_for_validation.R
+++ extract_for_validation.R
@@ -1,19 +1,22 @@
-# install.packages("ncdf4")
-# install.packages("ncdf4.helpers")
+#!/usr/bin/env Rscript
 
-# install.packages("PCICt")
-# install.packages("tidyr")
-# install.packages("ggplot2")
-# install.packages("readdr")
-# install.packages("ggplot2")
-# install.packages("ggplot2")
+## This script will read ncdf4 encoded WRF output files and extract
+## variables T2, RAINC and RAINNC for pairs of Lat, Lon given in a csv
+## file.
+##
+## Usage:
+##
+##     $ ./extract_for_validation.R weather_stations.csv wrfout_d04_2013-05-01_00\:00\:00 output.csv
+##
+## Where
+## - "weather_stations.csv" contains pairs of Lat, Lon in rows
+## - wrfout_d04_2013-05-01_00\:00\:00 is a ncdf4 encoded file output by WRF
+## - output.csv is where triads of T2, RAINC, RAINNC will be saved, in
+##   a single row, one for each pair of Lat, Lon given at input
+##
 
 library(ncdf4)
 library(ncdf4.helpers)
-## library(PCICt)
-## library(dplyr)
-## library(tidyr)
-## library(ggplot2)
 
 args <- commandArgs(TRUE)
 
@@ -26,8 +29,13 @@ temp <- ncvar_get(climate_output, varid = "T2")
 pp0 <- ncvar_get(climate_output, varid = "RAINC")
 pp1 <- ncvar_get(climate_output, varid = "RAINNC")
 
+
+out <- c()
+
 for (x in 1:length(estaciones[,1])){
   column <- match(round(estaciones[x ,1], digits=2), round(lat[1,], digits=2))
   row <- match(round(estaciones[x, 2], digits=2), round(lon[,1], digits=2))
-  print(c(temp[row, column], pp0[row, column], pp1[row, column]))
+  out <- append(out, (c(temp[row, column], pp0[row, column], pp1[row, column])))
 }
+
+write.table(t(unlist(out)), file=args[3], row.names=FALSE, col.names=FALSE)
