c3382d4d597e195f24645550cd0faa3e00542f81
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-03-12 15:57:14 -0600

    paths to netcdf and stations from arguments

--- extract_for_validation.R
+++ extract_for_validation.R
@@ -10,14 +10,14 @@
 library(ncdf4)
 library(ncdf4.helpers)
 library(PCICt)
-# library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-estaciones <- read.csv("Estaciones_WRF.csv")
-estaciones <- estaciones 
-climate_output <- nc_open("wrfout_d04_2013-01-01_00%3A00%3A00")
+args <- commandArgs(TRUE)
+
+estaciones <- read.csv(args[1])
+climate_output <- nc_open(args[2])
 
 lon <- ncvar_get(climate_output, varid = "XLONG")
 lat <- ncvar_get(climate_output, varid = "XLAT")
@@ -25,14 +25,8 @@ temp <- ncvar_get(climate_output, varid = "T2")
 pp0 <- ncvar_get(climate_output2, varid = "RAINC")
 pp1 <- ncvar_get(climate_output2, varid = "RAINNC")
 
-
-TablaWRF <- c()
-
 for (x in 1:length(estaciones[,1])){
-  column <- match(round(estaciones[x,1],digits=2),round(lat[1,],digits = 2))
-  row <- match(round(estaciones[x,2],digits=2),round(lon[,1],digits = 2))
-  TablaWRF <- append(TablaWRF,temp[row,column])
-  TablaWRF <- append(TablaWRF,pp0[row,column])
-  TablaWRF <- append(TablaWRF,pp1[row,column])
-  print(c(x, column,temp[row,column]))
+  column <- match(round(estaciones[x ,1], digits=2), round(lat[1,], digits=2))
+  row <- match(round(estaciones[x, 2], digits=2), round(lon[,1], digits=2))
+  print(c(temp[row, column], pp0[row, column], pp1[row, column]))
 }
